0d90afe27f4dc29c491e86f7ed17ff9c55c2e1de
Caio <caioagralemos@live.com>
2024-07-09 15:49:08 -0300

    Initial Commit

---
+++ Cosmic/CosmicApp.swift
@@ -0,0 +1,17 @@
+//
+//  CosmicApp.swift
+//  Cosmic
+//
+//  Created by Caio on 09/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CosmicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
